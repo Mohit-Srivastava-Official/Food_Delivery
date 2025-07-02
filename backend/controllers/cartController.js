29d9abc172b5cedc330592b27fa9ba1ef3498d4b
Mohit Srivastava <mohitsrivastavaofficial@gmail.com>
2025-07-02 10:06:23 +0530

    Updated cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,52 +1,53 @@
-import userModel from "../models/userModel.js"
+import userModel from "../models/userModel.js"  // Importing user model
 
-// add to user cart  
+// Adding item to user's cart
 const addToCart = async (req, res) => {
    try {
-      let userData = await userModel.findOne({_id:req.body.userId});
-      let cartData = await userData.cartData;
+      let userData = await userModel.findOne({ _id: req.body.userId });  // Fetching user data
+      let cartData = await userData.cartData;  // Accessing user's cart
+
       if (!cartData[req.body.itemId]) {
-         cartData[req.body.itemId] = 1;
+         cartData[req.body.itemId] = 1;  // Adding new item with quantity 1
+      } else {
+         cartData[req.body.itemId] += 1;  // Increasing item quantity
       }
-      else {
-         cartData[req.body.itemId] += 1;
-      }
-      await userModel.findByIdAndUpdate(req.body.userId, {cartData});
-      res.json({ success: true, message: "Added To Cart" });
+
+      await userModel.findByIdAndUpdate(req.body.userId, { cartData });  // Saving updated cart
+      res.json({ success: true, message: "Added To Cart" });  // Sending success response
    } catch (error) {
-      console.log(error);
-      res.json({ success: false, message: "Error" })
+      console.log(error);  // Logging error
+      res.json({ success: false, message: "Error" });  // Sending error response
    }
 }
 
-// remove food from user cart
+// Removing item from user's cart
 const removeFromCart = async (req, res) => {
    try {
-      let userData = await userModel.findById(req.body.userId);
-      let cartData = await userData.cartData;
-      if (cartData[req.body.itemId] > 0) {
-         cartData[req.body.itemId] -= 1;
-      }
-      await userModel.findByIdAndUpdate(req.body.userId, {cartData});
-      res.json({ success: true, message: "Removed From Cart" });
-   } catch (error) {
-      console.log(error);
-      res.json({ success: false, message: "Error" })
-   }
+      let userData = await userModel.findById(req.body.userId);  // Fetching user data
+      let cartData = await userData.cartData;  // Accessing user's cart
 
+      if (cartData[req.body.itemId] > 0) {
+         cartData[req.body.itemId] -= 1;  // Decreasing item quantity
+      }
+
+      await userModel.findByIdAndUpdate(req.body.userId, { cartData });  // Saving updated cart
+      res.json({ success: true, message: "Removed From Cart" });  // Sending success response
+   } catch (error) {
+      console.log(error);  // Logging error
+      res.json({ success: false, message: "Error" });  // Sending error response
+   }
 }
 
-// get user cart
+// Getting user's cart
 const getCart = async (req, res) => {
    try {
-      let userData = await userModel.findById(req.body.userId);
-      let cartData = await userData.cartData;
-      res.json({ success: true, cartData:cartData });
+      let userData = await userModel.findById(req.body.userId);  // Fetching user data
+      let cartData = await userData.cartData;  // Accessing user's cart
+      res.json({ success: true, cartData: cartData });  // Sending cart data
    } catch (error) {
-      console.log(error);
-      res.json({ success: false, message: "Error" })
+      console.log(error);  // Logging error
+      res.json({ success: false, message: "Error" });  // Sending error response
    }
 }
 
-
-export { addToCart, removeFromCart, getCart }
+export { addToCart, removeFromCart, getCart }  // Exporting controller functions
