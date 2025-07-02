1a2376ee358774636936f0d6559c86cae8bd74a8
Mohit Srivastava <mohitsrivastavaofficial@gmail.com>
2025-07-02 11:00:06 +0530

    Updated foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -3,12 +3,12 @@ import { addFood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from 'multer';
 const foodRouter = express.Router();
 
-//Image Storage Engine (Saving Image to uploads folder & rename it)
+// Image Storage Engine (Saving image to 'uploads' folder & renaming it)
 
 const storage = multer.diskStorage({
-    destination: 'uploads',
+    destination: 'uploads',  // Saving uploaded files in 'uploads' folder
     filename: (req, file, cb) => {
-        return cb(null,`${Date.now()}${file.originalname}`);
+        return cb(null, `${Date.now()}${file.originalname}`);  // Renaming file with current timestamp + original name
     }
 })
 
