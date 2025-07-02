fdac927f944fa84dbcb36863a9401e25183638b6
Mohit Srivastava <mohitsrivastavaofficial@gmail.com>
2025-07-02 10:11:00 +0530

    Updated foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,55 +1,50 @@
-import foodModel from "../models/foodModel.js";
-import fs from 'fs'
+import foodModel from "../models/foodModel.js";  // Importing food model
+import fs from 'fs'  // Importing file system for deleting image files
 
-// all food list
+// Fetching all food items
 const listFood = async (req, res) => {
     try {
-        const foods = await foodModel.find({})
-        res.json({ success: true, data: foods })
+        const foods = await foodModel.find({})  // Getting all food documents
+        res.json({ success: true, data: foods })  // Sending food list as response
     } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
+        console.log(error);  // Logging error
+        res.json({ success: false, message: "Error" })  // Sending error response
     }
-
 }
 
-// add food
+// Adding a new food item
 const addFood = async (req, res) => {
-
     try {
-        let image_filename = `${req.file.filename}`
+        let image_filename = `${req.file.filename}`  // Getting uploaded image file name
 
         const food = new foodModel({
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            category:req.body.category,
-            image: image_filename,
+            name: req.body.name,  // Saving name
+            description: req.body.description,  // Saving description
+            price: req.body.price,  // Saving price
+            category: req.body.category,  // Saving category
+            image: image_filename,  // Saving image filename
         })
 
-        await food.save();
-        res.json({ success: true, message: "Food Added" })
+        await food.save();  // Saving food to database
+        res.json({ success: true, message: "Food Added" })  // Sending success response
     } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
+        console.log(error);  // Logging error
+        res.json({ success: false, message: "Error" })  // Sending error response
     }
 }
 
-// delete food
+// Deleting a food item
 const removeFood = async (req, res) => {
     try {
+        const food = await foodModel.findById(req.body.id);  // Finding food by ID
+        fs.unlink(`uploads/${food.image}`, () => { })  // Deleting image file from server
 
-        const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, () => { })
-
-        await foodModel.findByIdAndDelete(req.body.id)
-        res.json({ success: true, message: "Food Removed" })
-
+        await foodModel.findByIdAndDelete(req.body.id)  // Removing food from DB
+        res.json({ success: true, message: "Food Removed" })  // Sending success response
     } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
+        console.log(error);  // Logging error
+        res.json({ success: false, message: "Error" })  // Sending error response
     }
-
 }
 
-export { listFood, addFood, removeFood }
+export { listFood, addFood, removeFood }  // Exporting controller functions
